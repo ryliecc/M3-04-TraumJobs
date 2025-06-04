edd19d01375f4271b93256a60e5495652e305641
Rylie Castell <rylie.castell@gmail.com>
2025-06-05 01:03:58 +0200

    add favorites list

--- TraumJobs/Views/Favorites Views/FavoritesView.swift
+++ TraumJobs/Views/Favorites Views/FavoritesView.swift
@@ -5,11 +5,24 @@
 //  Created by Rylie Castell on 02.06.25.
 //
 
+import SwiftData
 import SwiftUI
 
 struct FavoritesView: View {
+    @Query(
+        filter: #Predicate<Job> { job in
+            job.isFavorite
+        }
+    ) var jobs: [Job]
+    
     var body: some View {
         Text("Favoriten")
+            .font(.headline)
+        List {
+            ForEach(jobs) { job in
+                Text(job.title)
+            }
+        }
     }
 }
 
