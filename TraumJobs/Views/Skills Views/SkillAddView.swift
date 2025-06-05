8dcecdc70903a19e05f2837a93a0edd87c18fb3a
Rylie Castell <rylie.castell@gmail.com>
2025-06-05 13:47:50 +0200

    add skill list and form

---
+++ TraumJobs/Views/Skills Views/SkillAddView.swift
@@ -0,0 +1,60 @@
+//
+//  SkillAddView.swift
+//  TraumJobs
+//
+//  Created by Rylie Castell on 05.06.25.
+//
+
+import SwiftUI
+
+struct SkillAddView: View {
+    @Environment(\.modelContext) var context
+    @Binding var sheetIsVisible: Bool
+    @State var newSkillTitle: String = ""
+    @State var newSkillLevel: SkillLevel = .medium
+    var body: some View {
+        VStack {
+            Form {
+                Section("Titel") {
+                    TextField("Titel", text: $newSkillTitle)
+                }
+                Section("Level") {
+                    Picker("Level", selection: $newSkillLevel) {
+                        ForEach(SkillLevel.allCases, id: \.rawValue) { level in
+                            Text(level.rawValue).tag(level)
+                        }
+                    }
+                    .pickerStyle(.wheel)
+                    .frame(height: 56)
+                }
+            }
+            HStack {
+                Button("Abbrechen") {
+                    resetForm()
+                    sheetIsVisible = false
+                }
+                Spacer()
+                Button("Hinzufügen") {
+                    addSkill()
+                    resetForm()
+                    sheetIsVisible = false
+                }
+            }
+        }
+    }
+    
+    private func resetForm() {
+        newSkillTitle = ""
+        newSkillLevel = .medium
+    }
+    
+    private func addSkill() {
+        let newSkill = Skill(title: newSkillTitle, level: newSkillLevel)
+        context.insert(newSkill)
+    }
+}
+
+#Preview {
+    @Previewable @State var sheetIsVisible: Bool = true
+    SkillAddView(sheetIsVisible: $sheetIsVisible)
+}
